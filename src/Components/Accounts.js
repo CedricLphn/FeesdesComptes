5c2dfbf211284c5b234a9f08ce33acf4403a43d3
Cédric <cedric.leprohon@yahoo.fr>
2019-06-04 17:50:57 +0200

    action button fix

--- src/Components/Accounts.js
+++ src/Components/Accounts.js
@@ -29,9 +29,6 @@ export class Accounts extends React.Component {
         </View>
               } />
 
-          </View>
-      <View style={{flex:1, backgroundColor: '#f3f3f3'}}>
-        {/* Rest of the app comes ABOVE the action button component !*/}
         <ActionButton buttonColor="rgba(231,76,60,1)">
           <ActionButton.Item buttonColor='#9b59b6' title="New Task" onPress={() => console.log("notes tapped!")}>
             <Icon name="md-create" style={styles.actionButtonIcon} />
@@ -43,7 +40,8 @@ export class Accounts extends React.Component {
             <Icon name="md-done-all" style={styles.actionButtonIcon} />
           </ActionButton.Item>
         </ActionButton>
-      </View>
+          </View>
+        {/* Rest of the app comes ABOVE the action button component !*/}
       </SafeAreaView>
     );
   }
