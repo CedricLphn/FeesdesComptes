1be0b23c9ce5466eeaf79dbfa3d61f6881633c1f
Cédric <cedric.leprohon@yahoo.fr>
2019-06-04 13:22:21 +0200

    Push action button test

--- src/Components/Accounts.js
+++ src/Components/Accounts.js
@@ -1,11 +1,46 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ActionButton from 'react-native-action-button';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export class Accounts extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up Accounts.js to start working on your app!</Text>
+          <View>
+              <Text>Mes comptes</Text>
+          </View>
+          <View>
+              <View> 
+                  <View> 
+                      <Text>Nom du compte</Text>
+                  </View>
+                  <View>
+                      <Text>Type</Text>
+                  </View>
+                  <View>
+                      <Text>1.000.000 €</Text>
+                  </View>
+              </View>
+              
+              
+              <View>
+              <View style={{flex:1, backgroundColor: '#f3f3f3'}}>
+        {/* Rest of the app comes ABOVE the action button component !*/}
+        <ActionButton buttonColor="rgba(231,76,60,1)">
+          <ActionButton.Item buttonColor='#9b59b6' title="New Task" onPress={() => console.log("notes tapped!")}>
+            <Icon name="md-create" style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+          <ActionButton.Item buttonColor='#3498db' title="Notifications" onPress={() => {}}>
+            <Icon name="md-notifications-off" style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+          <ActionButton.Item buttonColor='#1abc9c' title="All Tasks" onPress={() => {}}>
+            <Icon name="md-done-all" style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+        </ActionButton>
+      </View>
+              </View>
+          </View>
       </View>
     );
   }
@@ -15,7 +50,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  actionButtonIcon: {
+    fontSize: 20,
+    height: 22,
+    color: 'white',
+  }
 });
